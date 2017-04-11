0cb9cbc7b50853019eaafe20637c41b6244e5843
Karns <pkarns@deloitte.com>
2017-04-10 21:19:21 -0400

    have results from sql compared to prompt selection working. need to update sql next

--- bamazon.js
+++ bamazon.js
@@ -2,6 +2,8 @@
 var mysql = require("mysql");
 var prompt = require("prompt");
 
+var productChoiceArray = [];
+
 //creating connection to DB
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -24,17 +26,44 @@ connection.query("SELECT * FROM products", function(err, res){
 	console.log("------------BAMAZON PRODUCT LIST -----------");
 	for (i = 0; i < res.length; i++) {
 		console.log(res[i].item_id + "|" + res[i].product_name + "|" + res[i].department_name + "|" + res[i].price +
-		 "|" + res[i].stock_quantity);
+		 "|" + res[i].stock_quantity)
+		productChoiceArray.push(res[i].item_id);
+		console.log(productChoiceArray);
 	}//for loop end
-	prompt.get(["Id", "Units"], function(err, result) {
-		if (err) {
-			throw err;
-		}//if end
-		else {
-			console.log(result);
-		}//else end
+	prompt.get(["Product_Id", "Number_of_units"], function(err, result) {
+		// if (err) {
+		// 	throw err;
+		// }//if end
+		console.log(result.Product_Id);
+		var chosenProduct;
+		for (i = 0; i < res.length; i++) {
+			if (res[i].item_id == result.Product_Id && result.Number_of_units < res[i].stock_quantity) {
+				//chosenProduct = res[i];
+				//console.log(chosenProduct);
+				console.log("your order for " + result.Number_of_units + " units of " + res[i].product_name + " has been placed");
+			}//if end
+
+			// if (result.Number_of_units < chosenProduct.stock_quantity) {
+			// 	console.log("your order has been placed");
+			// }
+			// else {
+			// 	console.log("insufficient quantity");
+			// }
+			
+		}//for end
+		
+		//if the product id entered by the user equals one of the product 
+		// else if (result.Product_Id === ) {
+		// 	console.log(result.Number_of_units);
+		// }//else end
+		// else {
+		// 	console.log(result);
+		// 	//console.log("That's an invalid id, please select another option");
+		// }
 	});//result function end
 });//query function end
 prompt.start();
+
+connection.end();
 
 
